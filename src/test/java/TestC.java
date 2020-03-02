a30f47eb8ccce5ba50e1e267c85bf33c9f79571b
AlekseevArtem <tema9991@mail.ru>
2020-03-02 13:25:28 +0300

    Перегрузил метод и добавил тестов.

--- src/test/java/TestC.java
+++ src/test/java/TestC.java
@@ -24,7 +24,7 @@ public class TestC {
                 LocalDateTime.parse("10.01.2013 00:00:00", fmt), LocalDateTime.parse("20.01.2013 23:59:59", fmt), 99000);
         Cost third = new Cost (0, "6654", 1, 2,
                 LocalDateTime.parse("01.01.2013 00:00:00", fmt), LocalDateTime.parse("31.01.2013 00:00:00", fmt), 5000);
-        List<Cost> current = new ArrayList<Cost> () ;
+        List<Cost> current = new ArrayList<> () ;
         current.add(first);
         current.add(second);
         current.add(third);
@@ -35,7 +35,7 @@ public class TestC {
                 LocalDateTime.parse("15.01.2013 00:00:00", fmt), LocalDateTime.parse("25.01.2013 23:59:59", fmt), 92000);
         Cost thirdNew = new Cost (0, "6654", 1, 2,
                 LocalDateTime.parse("12.01.2013 00:00:00", fmt), LocalDateTime.parse("13.01.2013 00:00:00", fmt), 4000);
-        List<Cost> input = new ArrayList<Cost> () ;
+        List<Cost> input = new ArrayList<> () ;
         input.add(firstNew);
         input.add(secondNew);
         input.add(thirdNew);
@@ -52,7 +52,7 @@ public class TestC {
                 LocalDateTime.parse("01.01.2013 00:00:00", fmt), LocalDateTime.parse("20.02.2013 23:59:59", fmt), 11000);
         Cost sixthResult = new Cost (0, "122856", 2, 1,
                 LocalDateTime.parse("15.01.2013 00:00:00", fmt), LocalDateTime.parse("25.01.2013 23:59:59", fmt), 92000);
-        List<Cost> expect = new ArrayList<Cost>();
+        List<Cost> expect = new ArrayList<>();
         expect.add(firstResult);
         expect.add(secondResult);
         expect.add(thirdResult);
@@ -60,6 +60,61 @@ public class TestC {
         expect.add(fifthResult);
         expect.add(sixthResult);
         List<Cost> result = BigCode.addNewCosts(current , input);
+        Assert.assertThat(result, is(expect));
+    }
+
+    @Test
+    public void TestForOneCostMethod_FromExample1() {
+        //current
+        Cost first = new Cost(0, "2345", 1, 1,
+                LocalDateTime.parse("01.01.2013 00:00:00", fmt), LocalDateTime.parse("28.01.2013 00:00:00", fmt), 80);
+        //input
+        Cost firstNew = new Cost(0, "2345", 1, 1,
+                LocalDateTime.parse("05.01.2013 00:00:00", fmt), LocalDateTime.parse("16.01.2013 00:00:00", fmt), 90);
+        List<Cost> currentNew = new ArrayList<>();
+        currentNew.add(firstNew);
+        //result
+        Cost firstResult = new Cost(0, "2345", 1, 1,
+                LocalDateTime.parse("01.01.2013 00:00:00", fmt), LocalDateTime.parse("05.01.2013 00:00:00", fmt), 80);
+        Cost secondResult = new Cost(0, "2345", 1, 1,
+                LocalDateTime.parse("05.01.2013 00:00:00", fmt), LocalDateTime.parse("16.01.2013 00:00:00", fmt), 90);
+        Cost thirdResult = new Cost(0, "2345", 1, 1,
+                LocalDateTime.parse("16.01.2013 00:00:00", fmt), LocalDateTime.parse("28.01.2013 00:00:00", fmt), 80);
+        List<Cost> expect = new ArrayList<>();
+        expect.add(firstResult);
+        expect.add(secondResult);
+        expect.add(thirdResult);
+        List<Cost> result = BigCode.addNewCosts(first, currentNew);
+        Assert.assertThat(result, is(expect));
+    }
+
+    @Test
+    public void TestFromExamples2() {
+        //current
+        Cost first = new Cost(0, "2345", 1, 1,
+                LocalDateTime.parse("01.01.2013 00:00:00", fmt), LocalDateTime.parse("16.01.2013 00:00:00", fmt), 100);
+        Cost second = new Cost(0, "2345", 1, 1,
+                LocalDateTime.parse("16.01.2013 00:00:00", fmt), LocalDateTime.parse("31.01.2013 00:00:00", fmt), 120);
+        List<Cost> current = new ArrayList<>();
+        current.add(first);
+        current.add(second);
+        //input
+        Cost firstNew = new Cost(0, "2345", 1, 1,
+                LocalDateTime.parse("07.01.2013 00:00:00", fmt), LocalDateTime.parse("24.01.2013 00:00:00", fmt), 110);
+        List<Cost> currentNew = new ArrayList<>();
+        currentNew.add(firstNew);
+        //result
+        Cost firstResult = new Cost(0, "2345", 1, 1,
+                LocalDateTime.parse("01.01.2013 00:00:00", fmt), LocalDateTime.parse("07.01.2013 00:00:00", fmt), 100);
+        Cost secondResult = new Cost(0, "2345", 1, 1,
+                LocalDateTime.parse("24.01.2013 00:00:00", fmt), LocalDateTime.parse("31.01.2013 00:00:00", fmt), 120);
+        Cost thirdResult = new Cost(0, "2345", 1, 1,
+                LocalDateTime.parse("07.01.2013 00:00:00", fmt), LocalDateTime.parse("24.01.2013 00:00:00", fmt), 110);
+        List<Cost> expect = new ArrayList<>();
+        expect.add(firstResult);
+        expect.add(secondResult);
+        expect.add(thirdResult);
+        List<Cost> result = BigCode.addNewCosts(current, currentNew);
         Assert.assertThat(result, is(expect));
     }
 
@@ -72,7 +127,7 @@ public class TestC {
                 LocalDateTime.parse("06.01.2013 00:00:00", fmt), LocalDateTime.parse("16.01.2013 00:00:00", fmt), 87);
         Cost third = new Cost(0, "2345", 1, 1,
                 LocalDateTime.parse("16.01.2013 00:00:00", fmt), LocalDateTime.parse("31.01.2013 00:00:00", fmt), 90);
-        List<Cost> current = new ArrayList<Cost>();
+        List<Cost> current = new ArrayList<>();
         current.add(first);
         current.add(second);
         current.add(third);
@@ -81,11 +136,10 @@ public class TestC {
                 LocalDateTime.parse("01.01.2013 00:00:00", fmt), LocalDateTime.parse("11.01.2013 00:00:00", fmt), 80);
         Cost secondNew = new Cost(0, "2345", 1, 1,
                 LocalDateTime.parse("11.01.2013 00:00:00", fmt), LocalDateTime.parse("21.01.2013 00:00:00", fmt), 85);
-        List<Cost> currentNew = new ArrayList<Cost>();
+        List<Cost> currentNew = new ArrayList<>();
         currentNew.add(firstNew);
         currentNew.add(secondNew);
-        List<Cost> result = BigCode.addNewCosts(current, currentNew);
-        System.out.println(result);
+
         //result
         Cost firstResult = new Cost(0, "2345", 1, 1,
                 LocalDateTime.parse("21.01.2013 00:00:00", fmt), LocalDateTime.parse("31.01.2013 00:00:00", fmt), 90);
@@ -93,11 +147,80 @@ public class TestC {
                 LocalDateTime.parse("01.01.2013 00:00:00", fmt), LocalDateTime.parse("11.01.2013 00:00:00", fmt), 80);
         Cost thirdResult = new Cost(0, "2345", 1, 1,
                 LocalDateTime.parse("11.01.2013 00:00:00", fmt), LocalDateTime.parse("21.01.2013 00:00:00", fmt), 85);
-        List<Cost> expect = new ArrayList<Cost>();
+        List<Cost> expect = new ArrayList<>();
         expect.add(firstResult);
         expect.add(secondResult);
         expect.add(thirdResult);
+        List<Cost> result = BigCode.addNewCosts(current, currentNew);
         Assert.assertThat(result, is(expect));
+    }
 
+    @Test
+    public void TestForOneCostMethod_CurrentBetweenTwoInputs() {
+        //current
+        Cost first = new Cost(0, "2345", 1, 1,
+                LocalDateTime.parse("01.01.2013 00:00:00", fmt), LocalDateTime.parse("06.01.2013 00:00:00", fmt), 80);
+        //input
+        Cost firstNew = new Cost(0, "2345", 1, 1,
+                LocalDateTime.parse("28.12.2012 00:00:00", fmt), LocalDateTime.parse("02.01.2013 00:00:00", fmt), 75);
+        Cost secondNew = new Cost(0, "2345", 1, 1,
+                LocalDateTime.parse("05.01.2013 00:00:00", fmt), LocalDateTime.parse("21.01.2013 00:00:00", fmt), 85);
+        List<Cost> currentNew = new ArrayList<>();
+        currentNew.add(firstNew);
+        currentNew.add(secondNew);
+        //result
+        Cost firstResult = new Cost(0, "2345", 1, 1,
+                LocalDateTime.parse("02.01.2013 00:00:00", fmt), LocalDateTime.parse("05.01.2013 00:00:00", fmt), 80);
+        Cost secondResult = new Cost(0, "2345", 1, 1,
+                LocalDateTime.parse("28.12.2012 00:00:00", fmt), LocalDateTime.parse("02.01.2013 00:00:00", fmt), 75);
+        Cost thirdResult = new Cost(0, "2345", 1, 1,
+                LocalDateTime.parse("05.01.2013 00:00:00", fmt), LocalDateTime.parse("21.01.2013 00:00:00", fmt), 85);
+        List<Cost> expect = new ArrayList<>();
+        expect.add(firstResult);
+        expect.add(secondResult);
+        expect.add(thirdResult);
+        List<Cost> result = BigCode.addNewCosts(first, currentNew);
+        Assert.assertThat(result, is(expect));
+    }
+
+    @Test
+    public void TestForOneCostMethod_WhenProduct_codesDifferent() {
+        //current
+        Cost first = new Cost(0, "2345", 1, 1,
+                LocalDateTime.parse("01.01.2013 00:00:00", fmt), LocalDateTime.parse("06.01.2013 00:00:00", fmt), 80);
+        //input
+        Cost firstNew = new Cost(0, "132213", 1, 1,
+                LocalDateTime.parse("28.12.2012 00:00:00", fmt), LocalDateTime.parse("02.01.2013 00:00:00", fmt), 75);
+        List<Cost> currentNew = new ArrayList<>();
+        currentNew.add(firstNew);
+        //result
+        Cost firstResult = new Cost(0, "2345", 1, 1,
+                LocalDateTime.parse("01.01.2013 00:00:00", fmt), LocalDateTime.parse("06.01.2013 00:00:00", fmt), 80);
+        Cost secondResult = new Cost(0, "132213", 1, 1,
+                LocalDateTime.parse("28.12.2012 00:00:00", fmt), LocalDateTime.parse("02.01.2013 00:00:00", fmt), 75);
+        List<Cost> expect = new ArrayList<>();
+        expect.add(firstResult);
+        expect.add(secondResult);
+        List<Cost> result = BigCode.addNewCosts(first, currentNew);
+        Assert.assertThat(result, is(expect));
+    }
+
+    @Test
+    public void TestForOneCostMethod_WhenSameExceptValues() {
+        //current
+        Cost first = new Cost(0, "2345", 1, 1,
+                LocalDateTime.parse("01.01.2013 00:00:00", fmt), LocalDateTime.parse("06.01.2013 00:00:00", fmt), 80);
+        //input
+        Cost firstNew = new Cost(0, "2345", 1, 1,
+                LocalDateTime.parse("01.01.2013 00:00:00", fmt), LocalDateTime.parse("06.01.2013 00:00:00", fmt), 90);
+        List<Cost> currentNew = new ArrayList<>();
+        currentNew.add(firstNew);
+        //result
+        Cost firstResult = new Cost(0, "2345", 1, 1,
+                LocalDateTime.parse("01.01.2013 00:00:00", fmt), LocalDateTime.parse("06.01.2013 00:00:00", fmt), 90);
+        List<Cost> expect = new ArrayList<>();
+        expect.add(firstResult);
+        List<Cost> result = BigCode.addNewCosts(first, currentNew);
+        Assert.assertThat(result, is(expect));
     }
 }
